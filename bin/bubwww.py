700a11090119d6f39bea436596a2b0497a2864e0
mjsqu <mj2.squ@gmail.com>
2021-03-17 23:35:48 +1300

    New program for www

--- bin/bubwww.py
+++ bin/bubwww.py
@@ -3,24 +3,16 @@
 # python3 -m http.server
 import webcambub as wc
 from time import sleep
-ferm1 = 0.00
-
-while True:
-    s = wc.openstream(1)
-
-
-    # Sleep for 4 mins 30
-    sleep(270)
-
+ferm1 = 0.005
 
 while True:
     data = s.read(wc.CHUNK)
     vol = rms(data)
     if loudest > ferm1:
-       with open(r'/home/pi/temperature/data/bubble.log','a+') as f:
-         f.write('ferm1|'+datetime.now().strftime(sf)+'\n')
+       with open(r'/home/pi/temperature/www/bubwww.log','a+') as f:
+         f.write('ferm1|'+datetime.now().strftime(wc.sf)+'\n')
  
 # stop Recording
-stream.stop_stream()
-stream.close()
-audio.terminate()
+s.stop_stream()
+s.close()
+wc.audio.terminate()
